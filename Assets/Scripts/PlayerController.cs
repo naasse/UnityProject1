c95302ec1b128de1917bb74c574793c0e89dbc22
naasse <nate.asselstine@gmail.com>
2017-09-14 19:57:41 -0400

    Pill bowling!!!!!!!!!!!
    Initial commit of some basic prefabs while I still figure some things out. Will branch soon once actually start working on an idea.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private float jumpPower;
+    public String jumpButton;
+
+    private bool isTouchingMap;
+    private Rigidbody rb;
+    private int count;
+    private int jumpPowerCount;
+    private int speedPowerCount;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 10.0f;
+        jumpPower = 0.0f;
+        count = 0;
+        jumpPowerCount = 0;
+        speedPowerCount = 1;
+    }
+
+    private void FixedUpdate()
+    {
+
+        float jumpNow = 0.0f;
+        //What if not 0 plane? Seems delayed, too - can't jump again for a few seconds after landing.
+        if (Input.GetKeyDown(jumpButton) && GetComponent<Rigidbody>().transform.position.y <= 0.55f)
+        {
+            jumpNow = jumpPower;
+        }
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), jumpNow, Input.GetAxis("Vertical"));
+        rb.AddForce(speed * movement);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("TAG NAME"))
+        {
+            //LOGIC
+        }
+        else  if (other.gameObject.CompareTag("Enemy"))
+        {
+            //LOGIC
+        }
+    }
+}
